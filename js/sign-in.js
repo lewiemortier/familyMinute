ef7f5f9276eda457034a445646f9f5aa24ff49db
Lewie Mortier <lewie.mortier@kahosl.be>
2014-04-01 15:59:00 +0200

    sign-in page works completely

--- js/sign-in.js
+++ js/sign-in.js
@@ -1,7 +1,45 @@
-$(document).on('pagebeforeshow', '#sign-in', function(){
-	console.log("button clicked");
-	$('#signIn').on('click',function(){
-		
-		$.mobile.changePage("#home");
+$(document).on('pagebeforeshow', '#signIn', function(){
+	
+	$('#navigationButtons .sign-in').on('click',function(){
+		$(".top").animate({'height':'40%'},500);
+		$(".photo").animate({'top':'25%'},500);
+		$(".slogan").animate({'top':'50%'},500);
+		$("#navigationButtons .sign-in").animate({'top':'60%'},500);
+		$(".sign-up").animate({'top':'60%'},500);
+		$(".bottom").animate({'height':'60%'},500);
+		$('#initial').fadeOut(500);
+		$('#loginCredentials').fadeIn(500);
 	});
+
+	$('#loginCredentials #submit').on('click', function(e){
+		e.preventDefault();
+		//check fields
+		var credentialsOk = true;
+		if ($('#username').val() == ""){
+			credentialsOk = false;
+		}
+
+		if ($('#password').val() == ""){
+			credentialsOk = false;
+		}
+
+		if (credentialsOk == false){
+			//show error message
+			alert("Unable to login");
+			$('#username').val("");
+			$('#password').val("");
+
+		}
+		else {
+			console.log('go to page home');
+			//go to home page
+			$.mobile.changePage("home.html");
+		}
+
+	});
+	//go to sign up
+	$('#navigationButtons .sign-up').on('click',function(){
+		$.mobile.changePage("sign-up.html");
+	});
+	
 });
